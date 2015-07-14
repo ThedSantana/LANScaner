633467204f031996444bff5ae02ef3e2cb6b7a96
liutao <taoliuh@gmail.com>
2015-07-14 17:13:48 +0800

    first commit.

---
+++ library/src/main/java/com/sonaive/library/ScanService.java
@@ -0,0 +1,19 @@
+package com.sonaive.library;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by liutao on 15-7-14.
+ */
+public class ScanService extends IntentService {
+
+    public ScanService() {
+        super("ScanService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
